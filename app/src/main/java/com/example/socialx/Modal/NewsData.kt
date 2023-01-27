c5e2fc2f3e8d660b7a186a2970ab58a3f697267e
Arihant Dugar <arihantdugar07@gmail.com>
2023-01-27 19:12:42 +0530

    first commit

---
+++ app/src/main/java/com/example/socialx/Modal/NewsData.kt
@@ -0,0 +1,9 @@
+package com.example.socialx.Modal
+
+data class NewsData(
+    val publishedAt : String,
+    val source : String,
+    val title : String,
+    val desc : String,
+    val imageUrl : String
+)
